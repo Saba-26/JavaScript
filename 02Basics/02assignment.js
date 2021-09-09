1945d1d98738d3629070cccede68514e1626ebcd
Saba-26 <83002906+Saba-26@users.noreply.github.com>
2021-09-09 15:42:32 +0530

    simple signup form

---
+++ 02Basics/02assignment.js
@@ -0,0 +1,20 @@
+const uid="hdbf456";
+
+var firstName="Saba";
+var lastName="Dafedar";
+var email="saba@gamil.com";
+var password="78956";
+var confirmPassword="78956";
+var country="India";
+var state="Karnataka";
+var courseCount=0;
+var isLoggedInFromGoogle=false;
+var isLoggedInFromFacebook=false;
+var isLoggedInFromLinkedIn=false;
+
+console.log(`
+    User's Name =${firstName+" "+lastName}
+    User's email =${email}
+    Country=${country+","+state}
+    Logged In From Google=${isLoggedInFromGoogle};
+`);
---
+++ 02Basics/02variables.js
@@ -0,0 +1,20 @@
+const uid="abc123";
+
+var fullName="Saba Dafedar";
+var email="saba@gmail.com";
+var password="123456";
+var confirmPassword="123456";
+var courseCount=0;
+var isLoggedInFromGoogle=false;
+
+//fullname=prompt("Enter your Name"); -->To read data from user
+
+console.log(uid);
+console.log(fullName);
+
+console.log(`
+    With Unique Id:${uid}
+    User is : ${fullName}
+    and email is: ${email}
+    and password is:${password}
+`);
