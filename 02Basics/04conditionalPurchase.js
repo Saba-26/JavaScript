d3d0817ab4e6a3e1bb5777c71358a356d54610ab
Saba-26 <83002906+Saba-26@users.noreply.github.com>
2021-09-11 23:08:50 +0530

    and operator conditional JS

---
+++ 02Basics/04conditionalPurchase.js
@@ -0,0 +1,10 @@
+var isLoggedIn=true;
+var isEmailVerified=true;
+var cardInfo=true;
+
+if(isLoggedIn&&isEmailVerified&&cardInfo){
+    console.log("Allow user to make purchase");
+}
+else{
+    console.log("Not allowed");
+}
