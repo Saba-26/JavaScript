15325c1ce899a926c72ecf3a300d07c50ae2274c
Saba-26 <83002906+Saba-26@users.noreply.github.com>
2021-09-11 22:49:56 +0530

    temperature assignment

---
+++ Assignments/login.js
@@ -0,0 +1,13 @@
+var facebook=true;
+var google=false;
+var email=false;
+
+if(email){
+    console.log("Login Failed!!");
+}
+
+if(facebook){
+    console.log("Login Sucess!!");
+}else{
+    console.log("Login Failed");
+}
