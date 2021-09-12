bd2a44fd6c1c5d45d5b58a5380786b90bee0f6d5
Saba-26 <83002906+Saba-26@users.noreply.github.com>
2021-09-12 20:57:18 +0530

    login succesfull

--- 02Basics/07ConditionalSignout.js
+++ 02Basics/07ConditionalSignout.js
@@ -1,2 +1,10 @@
 //Show user a signout button if he is authenticated,
 //otherwise show him option to Login/Signup
+
+var authenticated=true;
+
+if(authenticated){
+    console.log("Show singout button");
+}else{
+    console.log("Show Login Options");
+}
