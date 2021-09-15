b3cbe1850cb680c3b397903cf1bc63c313fbfd7c
Saba-26 <83002906+Saba-26@users.noreply.github.com>
2021-09-15 20:18:58 +0530

    conditional roles

--- 02Basics/08ConditionalRoles.js
+++ 02Basics/08ConditionalRoles.js
@@ -3,3 +3,25 @@
 // subadmin - gets access to create/delte courses
 // testprep - gets access to create/delete tests
 // user - gets access to consume content
+
+
+var user="admin";
+
+switch (user) {
+    case "admin":
+        console.log("you get full access");
+        break;
+    case "subadmin":
+        console.log("gets access to create/delte courses");
+        break;
+    case "testprep":
+        console.log("gets access to create/delete tests");
+        break;
+    case "user":
+        console.log("gets access to consume content");
+        break;    
+        
+
+    default:
+        break;
+}
