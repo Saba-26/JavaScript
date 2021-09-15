1ccf5536fb49125f6485e53bc3480635e8456951
Saba-26 <83002906+Saba-26@users.noreply.github.com>
2021-09-15 20:43:27 +0530

    truthy and falsy condition

---
+++ 02Basics/09TruthyAndFalsy.js
@@ -0,0 +1,24 @@
+// TODO:falsey
+// undefined
+// null
+// " "
+// NaN
+// 0
+
+var user=" ";
+
+if(user){
+    console.log("Condition is true");
+}
+
+console.log(2+"2");
+
+var sub="2";
+if(2==sub){
+    console.log("true");
+}
+
+var sub="2";
+if(2===sub){
+    console.log("true");
+}
