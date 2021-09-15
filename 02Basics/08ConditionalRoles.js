9a3696acca3903eb3173bd0cecfe50ff04e31041
Saba-26 <83002906+Saba-26@users.noreply.github.com>
2021-09-15 19:58:51 +0530

    listed rules

---
+++ 02Basics/08ConditionalRoles.js
@@ -0,0 +1,5 @@
+//Create an application with following rules
+// admin - gets full access
+// subadmin - gets access to create/delte courses
+// testprep - gets access to create/delete tests
+// user - gets access to consume content
