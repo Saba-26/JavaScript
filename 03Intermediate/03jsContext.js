104d8f8b6a1f52b44ee400fc44d0603a43ddbd94
Saba-26 <83002906+Saba-26@users.noreply.github.com>
2021-09-22 19:50:43 +0530

    context js

---
+++ 03Intermediate/03jsContext.js
@@ -0,0 +1,15 @@
+sayHello();
+
+function sayHello() {
+    console.log("hello");
+}
+
+if(2===2){
+    console.log("This is true");
+}
+
+var myName="saba";
+
+if (myName===window.myName) {
+    console.log("this is true");
+}
