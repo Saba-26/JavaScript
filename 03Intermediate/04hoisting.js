00bd51427f9d1f085946bc8be90594c7d9134588
Saba-26 <83002906+Saba-26@users.noreply.github.com>
2021-09-22 22:45:17 +0530

    hoisting example

---
+++ 03Intermediate/04hoisting.js
@@ -0,0 +1,31 @@
+// function tipper(a){
+//     var bill=a;
+//     console.log(bill+5);
+// }
+
+tipper("10");
+function tipper(a){
+    var bill=parseInt(a);
+    console.log(bill+5);
+}
+
+
+
+var bigTipper=function (b){
+    var bill=parseInt(b);
+    console.log(bill+15);
+}
+bigTipper("200");
+
+
+console.log(name);
+var name="saba";
+
+
+function sayName(){
+    var name="Mr. H";
+    console.log(name);
+}
+sayName();
+
+console.log(name);
