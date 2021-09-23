9c6fd90edf0dd01820a19efed71dc12d389325dc
Saba-26 <83002906+Saba-26@users.noreply.github.com>
2021-09-24 01:43:23 +0530

    objects

---
+++ 03Intermediate/10Objects.js
@@ -0,0 +1,10 @@
+var user = {
+    firstName:"Saba",
+    lastName:"Dafedar",
+    role:"Admin",
+    loginCount:32,
+    facebookSignedIn:true
+};
+
+console.log(user.firstName);
+console.log(user["lastName"]);
