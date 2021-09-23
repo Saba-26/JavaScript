0b1072c84aabde94d19505143feeec7422e3d024
Saba-26 <83002906+Saba-26@users.noreply.github.com>
2021-09-23 16:29:17 +0530

    initial commit

---
+++ Namaste-JS/hoisting.js
@@ -0,0 +1,20 @@
+getName();
+console.log(x);
+
+
+console.log(getName);
+
+var x=7;
+function getName(){
+    console.log("Hello JS");
+}
+
+// Name();
+// console.log(Name);
+
+// var Name=()=>{
+//     console.log("Hello");
+// }
+
+
+
