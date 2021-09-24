ce10dcb6cf1c098c71fba0e2a37e2edaf7f5969e
Saba-26 <83002906+Saba-26@users.noreply.github.com>
2021-09-24 15:07:29 +0530

    objects assignment

--- 03Intermediate/10Objects.js
+++ 03Intermediate/10Objects.js
@@ -8,3 +8,9 @@ var user = {
 
 console.log(user.firstName);
 console.log(user["lastName"]);
+
+console.log(user.loginCount);
+user.loginCount=44;
+console.log(user.loginCount);
+console.table(user);
+
---
+++ Assignments/objects-asign.js
@@ -0,0 +1,9 @@
+var iPhone={
+    batterBackup:'17 hours',
+    camera:'Ultra Wide',
+    screenSize:'13.7 cm',
+    display:'LCD display',
+    frontCamera:'Single wide camera'
+};
+
+console.table(iPhone);
