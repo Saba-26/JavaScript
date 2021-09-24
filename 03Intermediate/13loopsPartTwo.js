ce315df7a3766e481aeb6f164621f49d806fa3cf
Saba-26 <83002906+Saba-26@users.noreply.github.com>
2021-09-24 19:58:04 +0530

    for each loop

---
+++ 03Intermediate/13loopsPartTwo.js
@@ -0,0 +1,24 @@
+const myStates = ["Rajasthan","New Delhi","Assam",1947,"Goa","Karnataka","Maharastra"];
+
+
+myStates.forEach((s)=>console.log(s));
+
+
+
+
+
+
+
+
+
+
+
+
+// var i=0;
+
+// for (;;){
+//     if(i>3)
+//       break;
+//     console.log(i);
+//     i++;  
+// }
